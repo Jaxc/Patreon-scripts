54f1e178065344815a5a276ce7bc0ac3e9805c06
Jacob Rosén <jacob.rosen@volvocars.com>
2019-06-18 10:09:26 +0200

    add functionality to change sorting column

--- find_anniversary.py
+++ find_anniversary.py
@@ -2,7 +2,7 @@
 # Patron name converter
 # Made by Jaxcie
 #
-# This script can be used to convert a csv output from patreon and convert 
+# This script can be used to convert a csv output from patreon and convert
 # this to a txt file with columns.
 ###############################################################################
 
@@ -37,64 +37,64 @@ if default_input == 'Y':
 this_month = datetime.today().replace(day=1)
 last_month = this_month - timedelta(days=1)
 next_month = this_month + timedelta(days=32)
-	
+
 # Open input file
 with open(input_file, 'r') as csv_file:
 	# Open target file (will be overwritten)
-	with open('anniversary_out.txt', 'w', newline='') as file_out:
+	with open('patreon_anniversary.txt', 'w', newline='') as file_out:
 		# Read CSV data
 		csv_data = csv.reader(csv_file, delimiter=',')
-		
+
 		# Extract lists of users with anniversaries
 		last_month_anniversarys = []
 		this_month_anniversarys = []
 		next_month_anniversarys = []
-		
+
 		for row in csv_data:
 			# Extract name
 			name = row[0] + ' ' + row[1]
 			# Extract patreon start date
 			patron_start = datetime.strptime(row[12], "%Y-%m-%d %H:%M:%S.%f")
 			# Make sure patreon is still subscribed
-			try : 
+			try :
 				if((float(row[3]) > 0) and (float(row[4]) > 0) and (row[5] == 'Ok')):
 					# Compare start against month
 					if patron_start.month == last_month.month :
 						# Calculate how many years since start
 						years_active = str(last_month.year - patron_start.year)
-						last_month_anniversarys.append(years_active + 
-						" year(s) on " + str(patron_start.month) + "-" + 
+						last_month_anniversarys.append(years_active +
+						" year(s) on " + str(patron_start.month) + "-" +
 						str(patron_start.day) + ": " + name + '\r\n')
-						
+
 					elif patron_start.month == this_month.month :
 						years_active = str(this_month.year - patron_start.year)
-						this_month_anniversarys.append(years_active + 
-						" year(s) on " + str(patron_start.month) + "-" + 
+						this_month_anniversarys.append(years_active +
+						" year(s) on " + str(patron_start.month) + "-" +
 						str(patron_start.day) + ": " + name + '\r\n')
-						
+
 					elif patron_start.month == next_month.month :
 						years_active = str(next_month.year - patron_start.year)
-						next_month_anniversarys.append(years_active + 
-						" year(s) on " + str(patron_start.month) + "-" + 
+						next_month_anniversarys.append(years_active +
+						" year(s) on " + str(patron_start.month) + "-" +
 						str(patron_start.day) + ": " + name + '\r\n')
 			except:
 				# Error in row, could not parse
-				print(row)	
-			
+				print(row)
+
 		# Format the list in to columns and write to file
 		file_out.write("Anniversaries this month: \r\n")
 		for name in this_month_anniversarys:
 			file_out.write(name)
 		file_out.write('\r\n')
-		
+
 		file_out.write("Anniversaries last month: \r\n")
 		for name in last_month_anniversarys:
-			file_out.write(name)		
+			file_out.write(name)
 		file_out.write('\r\n')
-		
+
 		file_out.write("Anniversaries next month: \r\n")
 		for name in next_month_anniversarys:
-			file_out.write(name)		
-		
+			file_out.write(name)
+
 		# Add final line break
 		file_out.write('\r\n')
--- parse_patron.py
+++ parse_patron.py
@@ -2,7 +2,7 @@
 # Patron name converter
 # Made by Jaxcie
 #
-# This script can be used to convert a csv output from patreon and convert 
+# This script can be used to convert a csv output from patreon and convert
 # this to a txt file with columns.
 ###############################################################################
 
@@ -20,19 +20,25 @@ import glob
 # import datetime to determine newest files
 import datetime
 # import shared functions
-from patreon_support_functions import * 
+from patreon_support_functions import *
+
+
+def sortSecond(val):
+    return val[1]
+
 
 # Read parameters from config.ini
 default_input = read_config_param('INPUT', 'DEFAULT_INPUT')
 input_file = read_config_param('INPUT', 'INPUT_FILE')
 name_spacing = int(read_config_param('PARSE_PATREON', 'NAME_SPACING'))
-n_col = int(read_config_param('PARSE_PATREON','N_COL'))
+n_col = int(read_config_param('PARSE_PATREON', 'N_COL'))
+output_sorting = read_config_param('PARSE_PATREON', 'OUTPUT_SORTING')
+output_sorting_order = read_config_param('PARSE_PATREON', 'OUTPUT_SORTING_ORDER')
 
-
+# Find latest CSV file or override it with INPUT_FILE
 if default_input == 'Y':
 	path=os.getcwd()
 	csv_files = glob.glob('*.{}'.format('csv'))
-	patreon_reports_name = []
 	patreon_reports_date = []
 	for csv_file in csv_files :
 		standard_csv = re.search('PatreonReport_(\d{4}_\d{2}_\d{2})\.csv', csv_file)
@@ -41,19 +47,33 @@ if default_input == 'Y':
 	newest_date = max(patreon_reports_date)
 	input_file = newest_date.strftime("PatreonReport_%Y_%m_%d.csv")
 
+# Decode the sorting order
+sorting_ways = ['NAME', '', 'CURRENT_PLEDGE', 'LIFETIME_PLEDGE', '', '', '', '', '', '', 'COUNTRY', 'FIRST_PLEDGE_DATE', 'MAX_PLEDGED_AMOUNT']
+
+for i in range(len(sorting_ways)) :
+	if sorting_ways[i] == output_sorting:
+		sort_col = i
+
+if output_sorting_order == 'ASCENDING' :
+	reverse = False
+elif output_sorting_order == 'DECENDING' :
+	reverse = True
+else :
+	raise exception ('Key OUTPUT_SORTING_ORDER in PARSE_PATREON is set incorrectly, please set it to either ASCENDING or DECENDING.')
+
 # Open input file
 with open(input_file) as csv_input_file:
 	# Open target file (will be overwritten)
-	with open('patons_out.txt', 'w', newline='') as file_out:
+	with open('Patreon_name_list.txt', 'w', newline='') as file_out:
 		# Read CSV data
 		csv_data = csv.reader(csv_input_file, delimiter=',')
-		
+
 		# Extract names and spacing to the list names
 		names = []
 		for row in csv_data:
 			try :
-				if((float(row[3]) > 0.0) 
-					and (float(row[4]) > 0.0) 
+				if((float(row[3]) > 0.0)
+					and (float(row[4]) > 0.0)
 					and (row[5] == 'Ok')):
 					# Concatenate first and last name
 					name = row[0] + ' ' + row[1]
@@ -63,25 +83,34 @@ with open(input_file) as csv_input_file:
 					# Spaces are inserted for uniform columns
 					name = '{:<{width}}'.format(name, width=name_spacing)
 
+					# Prepare row for sorting
+					row = [name, row[2], int(row[3]), int(row[4]),
+						row[5],  row[6],  row[7],  row[8],  row[9],  row[10],
+						row[11], row[12], int(row[13])]
 					# Add to list
-					names.append(name)
+					names.append([name, row])
 			except:
 				# Error in row, could not parse
 				print(row)
-			
-		i = 0	
+				print()
+
+		# Sort names according to output sorting
+		for i in range(len(names)):
+			names[i][1] = names[i][1][sort_col]
+		names.sort(key = sortSecond, reverse=reverse)
+		i = 0
 		# Format the list in to columns and write to file
 		for i in range(0, len(names) - n_col + 1, n_col) :
 			for j in range(i, i + n_col):
-				file_out.write(names[j])
+				file_out.write(names[j][0])
 			file_out.write("\r\n")
-		
+
 		# Fix last row if number of names if it isn't evently divided by n_col
 		if (len(names) < n_col) :
 			for j in range(i, len(names)):
-				file_out.write(names[j])
+				file_out.write(names[j][0])
 		else :
 			for j in range(i + n_col, len(names)):
-				file_out.write(names[j])
+				file_out.write(names[j][0])
 		# Add final line break
 		file_out.write('\r\n')
